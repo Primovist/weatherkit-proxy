beb7f6f52aa423651205e692e92f2a81f885cef2
meme <meme@memestudio.dev>
2026-07-01 17:22:51 +0800

    chore: 重命名 Lodash.mjs 为 object.mjs
    因为里面的方法已经被精简为普通的 get/set/merge 函数，
    不再包含 Lodash 包装对象，改名为更准确的 object.mjs。

--- src/utils/index.mjs
+++ src/utils/index.mjs
@@ -1,6 +1,6 @@
 export * from "./Console.mjs";
 export * from "./context.mjs";
 export * from "./fetch.mjs";
-export * from "./Lodash.mjs";
+export * from "./object.mjs";
 
 export * from "./time.mjs";
